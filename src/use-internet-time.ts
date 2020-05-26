66770027d198e6469f24add4c0741338ef770c4f
Graham Barber <grahammbarber@gmail.com>
2020-05-26 14:39:02 -0700

    add ssr option

--- src/use-internet-time.ts
+++ src/use-internet-time.ts
@@ -3,14 +3,18 @@ import { now } from "dot-beat-time";
 
 type InternetTimeHookOptions = {
   fractional?: boolean;
+  ssr?: boolean;
 };
 
 function useInternetTime(options?: InternetTimeHookOptions) {
   const isFractional = options && options.fractional;
+  const ssr = options && options.ssr;
 
-  const [time, setTime] = useState(now(isFractional));
+  const [time, setTime] = useState(ssr ? "" : now(isFractional));
 
   useEffect(() => {
+    if (ssr) setTime(now(isFractional));
+
     const interval = setInterval(() => {
       setTime(now(isFractional));
     }, 864);
@@ -18,7 +22,7 @@ function useInternetTime(options?: InternetTimeHookOptions) {
     return () => {
       clearInterval(interval);
     };
-  }, [isFractional]);
+  }, [isFractional, ssr]);
 
   return time;
 }
